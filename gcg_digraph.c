452a483af4adc6abf2f56c175e9f3444622693e2
vpostman <kdaley3@student.gsu.edu>
2018-03-13 15:36:14 -0400

    we were running out of memory with 100 nodes

--- gcg_digraph.c
+++ gcg_digraph.c
@@ -25,7 +25,6 @@ void* alloc_seq( struct path_choice_queue* qseq, int nbatch, int N, int* qseq_he
 	struct path_choice_queue* q = &( qseq[qhead0] );
 	if ( q->all_pairs_paths == NULL ) {
 		q->all_pairs_paths = malloc( N * N * diam * sizeof( int ) );
-		memset( q->all_pairs_paths,255,N * N * diam * sizeof( int ) );
 	}
 	return q;
 }
@@ -189,6 +188,7 @@ float run_connection_graph_method_for_path_choice( struct path_choice_queue* q,
 					int v = q->all_pairs_paths[( i * N + j ) * diam + k + 1];
                     //reroute the edge if it is a bottleneck
 					if ( lhs[v * N + u] >= lhsmax - 0.0000001 ) {
+						reroute_edge[v * N + u] = 1;
 						reroute_edge[u * N + v] = 1;
 					}
 				}
@@ -265,6 +265,8 @@ float run_connection_graph_method_for_path_choice( struct path_choice_queue* q,
 	}
 }
 struct connection_graph_params_t {//parameters for iterating over the queue
+    float alpha;
+    int queue_imbalance;
 	void* qv, *qseq;
 	int nbatch;
 	volatile float* epsilon_min;
@@ -294,16 +296,27 @@ void* run_connection_graph_optimization( void* pparms ) {//optimization routine
 	struct path_choice_queue* qHead0 = parms->qHead0;
 	struct path_choice_queue* q = qv;
 	int* reroute_edge = malloc( N * N * sizeof( int ) );
+    --parms->queue_imbalance;
 	int ktoj, itoj, itok;
 	while ( q != NULL ) {
 		memset( reroute_edge,0,N * N * sizeof( int ) );
 		float epsilon = run_connection_graph_method_for_path_choice( q, reroute_edge, mean_node_unbalance, twocell, N, directed, Aaug );//compute our current bound
 		epsilon_min[0] = MIN( epsilon_min[0], epsilon );//compare it to our current best bound
-		if ( epsilon < 2 * ( epsilon_min[0] ) ) {//this backtracking criterion is optimal in the case of uniformly weighted, 
+		if ( epsilon <= parms->alpha*( epsilon_min[0] ) ) {//this backtracking criterion is optimal for alpha=2 in the case of uniformly weighted, 
                                                 //node-balanced networks; see the paper for more details.
 			int i,j;
+            int ii,jj,kk;
+            for ( ii = 0; ii < N; ++ii ) {
+                for ( jj = 0; jj < N; ++jj) {
+                    int m=jj;
+                    for ( kk = 1; kk < diam && m!=ii; ++kk) {
+                        m=q->all_pairs_paths[(ii*N+jj)*diam+kk];
+                        parms->rerouted[(ii*N+jj)*N+m]=1;
+                    }
+                }
+            }
 			for ( i = 0; i < N; ++i ) {
-				for ( j = 0; j < N; ++j ) {
+				for ( j = i+1; j < N; ++j ) {
                     
 					for ( itoj = 0; itoj < diam && q->all_pairs_paths[( i * N + j ) * diam + itoj] != i; ++itoj ) ;
                     
@@ -311,6 +324,10 @@ void* run_connection_graph_optimization( void* pparms ) {//optimization routine
 						continue;
 					}//if there is no path from i to j we have no way to reroute.
 					
+					if(itoj <= 2 ){
+                        continue;
+                    }
+                    
 					int d;
 					int reroute = 0;
 					for ( d = 0; d < itoj; ++d ) {//we consider whether we ought to reroute along this path (based on                                
@@ -321,7 +338,6 @@ void* run_connection_graph_optimization( void* pparms ) {//optimization routine
 							reroute = 1;
 							break;
 						}
-
 					}
 					if ( !reroute ) {
 						continue;
@@ -331,12 +347,25 @@ void* run_connection_graph_optimization( void* pparms ) {//optimization routine
 					for ( k = 0; k < N; ++k ) {
                         //our next iteration consists of the concatenated path $P_{ik} + P_{kj}$
                         //compute this new path
-						if ( parms->rerouted[i * N * N + j * N + k] ) {
-							continue;
-						}
+                        
 						if ( k == i || k == j ) {
 							continue;
 						}
+						
+						if ( parms->rerouted[i * N * N + j * N + k] || parms->rerouted[j * N * N + i * N + k]) {
+							continue;
+						}
+						
+						int adjacent=0;
+                        
+						for ( itoj = 0; itoj < diam && q->all_pairs_paths[( i * N + j ) * diam + itoj] != i; ++itoj ) {
+                            if(Aaug[k,q->all_pairs_paths[( i * N + j ) * diam + itoj]]>0){
+                                adjacent=1;
+                            }
+                        }
+                        if(adjacent==0){
+                            continue;
+                        }
 						for ( itok = 0; itok < diam && q->all_pairs_paths[( i * N + k ) * diam + itok] != i; ++itok ) ;
 						for ( ktoj = 0; ktoj < diam && q->all_pairs_paths[( k * N + j ) * diam + ktoj] != k; ++ktoj ) ;
 						if ( itok == diam ) {
@@ -345,7 +374,6 @@ void* run_connection_graph_optimization( void* pparms ) {//optimization routine
 						if ( ktoj == diam ) {
 							continue;
 						}
-						int use = 1;
 						for ( d = 0; d < ktoj; ++d ) {
 							int ire = q->all_pairs_paths[( k * N + j ) * diam + d];
 							int jre = q->all_pairs_paths[( k * N + j ) * diam + d + 1];
@@ -354,20 +382,15 @@ void* run_connection_graph_optimization( void* pparms ) {//optimization routine
 							int ire = q->all_pairs_paths[( i * N + k ) * diam + d];
 							int jre = q->all_pairs_paths[( i * N + k ) * diam + d + 1];
 						}
-						if ( !use ) {
-							continue;
-						}
 
-						if ( !directed && ( N / twocell ) * epsilon_min[0] * 2 <= itoj + ktoj - 1.0000000001 ) {
+						if (!directed && ( N / twocell ) * epsilon_min[0] * parms->alpha <= itoj + ktoj - 1.0000000001 ) {
 							continue;
 						}
 
                         //and add it to the queue
-						struct path_choice_queue* qHead = alloc_seq( qseq, nbatch,N, qseq_head );
+						struct path_choice_queue* qHead = alloc_seq( qseq, nbatch, N, qseq_head );
 						qHead->next = NULL;
 
-						int kk;
-                        int ii,jj;
 						for ( ii = 0; ii < N; ++ii ) {
                             for ( jj = 0; jj < N; ++jj) {
                                 int m=jj;
@@ -380,27 +403,27 @@ void* run_connection_graph_optimization( void* pparms ) {//optimization routine
 						
 						for ( kk = 0; kk <= itok; ++kk ) {
                             
-                            int m=q->all_pairs_paths[( k * N + j ) * diam + kk];
-                            parms->rerouted[i * N * N + j * N + m] = 1;
-							qHead->all_pairs_paths[( i * N + j ) * diam + kk + ktoj] = q->all_pairs_paths[( i * N + k ) * diam + kk];
+                            int m=q->all_pairs_paths[( i * N + k ) * diam + kk];
+							qHead->all_pairs_paths[( i * N + j ) * diam + kk + ktoj] = m;
 						}
 						for ( kk = 0; kk <= ktoj - 1; ++kk ) {
                             int m=q->all_pairs_paths[( k * N + j ) * diam + kk];
-                            parms->rerouted[i * N * N + j * N + m] = 1;
-							qHead->all_pairs_paths[( i * N + j ) * diam + kk] = q->all_pairs_paths[( k * N + j ) * diam + kk];
+							qHead->all_pairs_paths[( i * N + j ) * diam + kk] = m;
 						}
 						parms->rerouted[i * N * N + j * N + k] = 1;
 						//and to the visited configuration list
 						qHead->next = q->next;
 						struct path_choice_queue* qHead0 = q;
 						qHead0->next = qHead;
-
-
+                        ++parms->queue_imbalance;
+                        
 					}
 
 				}
 			}
 		}
+		fprintf(stderr,"%i %f\n", parms->queue_imbalance, epsilon_min[0]); 
+        //fflush(stderr);
 		//keep iterating until we have nothing left to examine.
 		q = q->next;
 
@@ -411,7 +434,7 @@ void* run_connection_graph_optimization( void* pparms ) {//optimization routine
 	return NULL;
 }
 
-float generalized_connection_graph_method( float* A, float a, int num_nodes, int augment, int nbatch ) {
+float generalized_connection_graph_method( float* A, float a, int num_nodes, int augment, int nbatch, float alpha) {
 	int N = num_nodes;
 	int diam = N;
 	struct path_choice_queue* qseq = malloc( nbatch * sizeof( struct path_choice_queue ) );
@@ -542,6 +565,7 @@ float generalized_connection_graph_method( float* A, float a, int num_nodes, int
 	float epsilon_min = FLT_MAX;
 	struct connection_graph_params_t parms =
 	{
+        alpha,1,
 		q1, qseq,
 		nbatch,
 		&epsilon_min,
@@ -582,7 +606,7 @@ float generalized_connection_graph_method( float* A, float a, int num_nodes, int
 
 int main( int argc, char** argv ) { ///example which produces fig. 3 from the augmented graph paper.
                                     //eigenvalue computation is via arpack.
-	int N = 100;
+	int N = 50;
 	int i,j, k;
 	Complex* workd = malloc( 3 * N * sizeof( Complex ) );//"working arrays" for arpack.
 	Complex* workl = malloc( ( 3 * N * N + 5 * N ) * sizeof( Complex ) );
@@ -593,14 +617,15 @@ int main( int argc, char** argv ) { ///example which produces fig. 3 from the au
     /*
 	for ( k = 5; k <= 50; ++k ) {
             memset( Adj,0,N * N * sizeof( float ) );
-            for ( i = 0; i < N; ++i ) {
+            for ( i = 1; i < N; ++i ) {
                 for ( j = 0; j < N; ++j ) {
                     if(j%50<k){
                         Adj[i*N+j]=1;
                     }
                 }
-            }*/
-    for ( k = 2; k <= 50; ++k ) {
+            }
+            */
+    for ( k = 2; k <= N/2; ++k ) {
             memset( Adj,0,N * N * sizeof( float ) );
             for ( i = 0; i < N; ++i ) {
                 for ( j = 1; j <=k; ++j ) {
@@ -615,18 +640,18 @@ int main( int argc, char** argv ) { ///example which produces fig. 3 from the au
 		//printf("\n");
 		float* Adj1 = malloc( N * N * sizeof( float ) );
 		memcpy( Adj1, Adj, N * N * sizeof( float ) );
-		float eps1 = generalized_connection_graph_method( Adj,7.79,N,1,1024 );
+		float eps1 = generalized_connection_graph_method( Adj,7.79,N,1,5020, 2.0);
         printf( "%i %f ",k,eps1);
-		float eps0 = generalized_connection_graph_method( Adj,7.79,N,0,1024 );
+		float eps0 = generalized_connection_graph_method( Adj,7.79,N,0,1, 0.99);
 		for ( i = 0; i < N; ++i ) {
-			Adj1[i] = Adj[i];
+			Adj1[i] = -Adj[i];
 			float sum = 0;
 			for ( j = 0; j < N; ++j ) {
 				sum += Adj1[j * N + i];
 			}
 			Adj1[i * N + i] = -sum;
 		}
-		int ido = 0, nev = 97;
+		int ido = 0, nev = N-3;
 		int ncv = N;
 		float tol = 0.000001;
 		int ldv = N;
@@ -635,7 +660,7 @@ int main( int argc, char** argv ) { ///example which produces fig. 3 from the au
 		int lworkl = 3 * N * N + 8 * N;
 		int info = 0;
 		char bmat = 'I';
-		char which[2] = {'L','R'};
+		char which[2] = {'S','M'};
 		while ( 1 ) {
 			cnaupd_( &ido, &bmat, &N, which,
 					 &nev, &tol, resid,
@@ -664,7 +689,7 @@ int main( int argc, char** argv ) { ///example which produces fig. 3 from the au
 			}
 		}
 		free(Adj1);
-		float re_eig2 = workl[ipntr[5] + 1][0];
+		float re_eig2 = workl[ipntr[5]+1][0];
 		int components = 0;
 		 printf( "%f %f %f\n", eps0,
 				7.79 / fabs( re_eig2 ), re_eig2 );
